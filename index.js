a83515c0821932b8c6e09939b1f15073cb19f7b3
Tom91011 <tomnightingale@hotmail.com>
2022-02-09 17:23:11 +0000

    register new users with mongoDB Atlas

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const PORT = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const session = require('express-session');
+dotenv.config();
+
+// Mongo DB conncetion
+const database = process.env.MONGODB_DATABASE_ACCESS;
+mongoose
+  .connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("e don connect"))
+  .catch((err) => console.log(err));
 
 app.set('view engine', 'ejs')
 app.use('/public', express.static(path.join(__dirname, './public')))
-// app.use('/public/images/');
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
+//BodyParsing
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret:'oneboy',
+    saveUninitialized: true,
+    resave: true
+  }));
+  
 
-app.get("/login",  (req, res) => {
-    res.render("login")
-})
+app.use('/', require('./routes/login'))
 
-app.get("/register", (req, res) => {
-    res.render("register")
-})
-app.listen(PORT, console.log("Server don start for port: " + PORT))
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, console.log("Server connected to port: " + PORT))
