44cde0dbcf1f00727609b2ed1b28fde2eac8cc10
Tom91011 <tomnightingale@hotmail.com>
2022-02-10 14:20:08 +0000

    add login functionality

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 const path = require('path')
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
-const session = require('express-session');
 dotenv.config();
+const session = require('express-session');
+const passport = require('passport')
+const { loginCheck } = require('./auth/passport')
+loginCheck(passport);
+
 
 // Mongo DB conncetion
 const database = process.env.MONGODB_DATABASE_ACCESS;
 mongoose
   .connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() => console.log("e don connect"))
+  .then(() => console.log("Connected to Mongoose"))
   .catch((err) => console.log(err));
 
 app.set('view engine', 'ejs')
@@ -23,7 +27,9 @@ app.use(session({
     saveUninitialized: true,
     resave: true
   }));
-  
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', require('./routes/login'))
 
