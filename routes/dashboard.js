f1609ef759b992ed5a1f176cdc0be809152fb17d
Tom91011 <tomnightingale@hotmail.com>
2022-02-11 16:12:40 +0000

    new article functionality now in compose page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,7 @@ const { addArticle } = require("../controllers/articleController")
 const { composeView } = require("../controllers/composeController")
 const router = express.Router();
 
-router.post("/new-article", addArticle);
+
 router.post("/compose", protectRoute, composeView)
 
 module.exports = router;
